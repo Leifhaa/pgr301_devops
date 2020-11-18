04a5398210fe2b5ebf425b5b652aae63554d48fc
Leifhaa <leifhaa@hotmail.com>
2020-11-18 22:25:40 +0100

    Added longlasting timer

--- src/main/kotlin/com/example/pgr301_devops/schedules/TasksScheduling.kt
+++ src/main/kotlin/com/example/pgr301_devops/schedules/TasksScheduling.kt
@@ -2,6 +2,7 @@ package com.example.pgr301_devops.schedules
 
 import com.example.pgr301_devops.data.TaskState
 import com.example.pgr301_devops.repository.TaskRepository
+import io.micrometer.core.annotation.Timed
 import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -14,9 +15,22 @@ import java.time.ZonedDateTime
 class TasksScheduling(
         private val repository: TaskRepository
 ){
-    @Scheduled(fixedDelay = 20000)
+
+
+    /* Scans for tasks is old and should be deleted*/
+    @Timed(value = "tasks.old.removal", longTask = true)
+    @Scheduled(fixedDelay = 60000)
     fun scanOldTasks() {
-        /* Scans for tasks is old and should be deleted*/
+        /*
+        * LongTask allows us to measure time WHEN the task is running unlike regular timer where it's measured afterwards.
+        * This is beneficial for tasks which takes long to execute.
+        *
+        * This method would usually execute instantly however I'd like to simulate the method as more complex and time consuming by simply
+        * adding a sleep. As the method now takes longer to execute, it also makes sense to have a longTask timer to see the duration
+        * of this method.
+        *  */
+        val rnds = (5..55).random() // generated random from 1 to 9 included
+        Thread.sleep(rnds.toLong() * 10000)
         val thres = ZonedDateTime.now().minusMinutes(1)
         repository.deleteAllByCreationTimeBeforeAndState(thres, TaskState.Completed)
     }
